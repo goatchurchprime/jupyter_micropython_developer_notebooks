76b648ce30855137b78bfa20bf912c0e582329c5
Julian Todd <julian@goatchurch.org.uk>
2017-11-14 20:56:55 +0000

    manage the basicsockets directory

--- basicsockets/connectwifi.py
+++ basicsockets/connectwifi.py
@@ -1,4 +1,5 @@
 import network
+from syslog import log
 
 si = network.WLAN(network.STA_IF)
 macaddress = "".join("{:02x}".format(x)  for x in si.config("mac"))
@@ -8,14 +9,25 @@ knownwifis = dict(k.split(b":")[:2]  for k in open("wificodes.txt", "rb"))
 print("Scanning wifis:", ", ".join(s.decode()  for s in knownwifis))
 
 # X[3]=RSSI (received signal strength)
-wifiname = max((sc  for sc in si.scan()  if sc[0] in knownwifis), key=lambda X: X[3])[0]  
+wifiname = max((sc  for sc in si.scan()  if sc[0] in knownwifis), 
+               key=lambda X: X[3], 
+               default=[None])[0]  
 
-print("Connecting to strongest known wifi signal", wifiname.decode())
-si.connect(wifiname, knownwifis[wifiname])
-del knownwifis
+if wifiname is not None:
+    log("Strongest wifi", wifiname.decode())
+    si.connect(wifiname, knownwifis[wifiname])
+    del knownwifis
 
-while not si.isconnected():
-    pass
+    while not si.isconnected():
+        pass
 
-ipnumber = si.ifconfig()[0]
-print("Device has ipnumber", ipnumber)
+    ipnumber = si.ifconfig()[0] 
+    
+else:
+    log("No recognized wifi")
+    si = network.WLAN(network.AP_IF) # create access-point interface
+    si.active(True)         # activate the interface
+    log("Creating access point ESP_{}".format(macaddress[-6:]))
+    ipnumber = si.ifconfig()[0]
+
+log("Device has ipnumber", ipnumber)
