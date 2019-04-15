78b62990b484cc69c4f6d71650bc1fb37ada5c1a
Julian Todd <julian@goatchurch.org.uk>
2019-04-15 19:48:39 +0100

    add comments to help with what happens next

--- projects/mqttmonitortext.py
+++ projects/mqttmonitortext.py
@@ -1,7 +1,26 @@
 import curses, time, re
 import paho.mqtt.client as mqtt
 
-# See https://docs.python.org/3/howto/curses.html
+# This is a special lightweight text display that can sit on the MQTT-broker to show its 
+# activity and help with debugging the system.  (A bit of a waste of a touchscreen, but we could replace it.)
+# You can run it on on any command line screen and it should resize
+# Inspired by https://github.com/jusplainmike/mqttmon but rewritten from scratch using https://docs.python.org/3/howto/curses.html
+
+# Recommends to use curses.wrapper() to avoid leaving your text terminal in an unusable state.  (This isn't working now!)
+
+# To do:
+#
+#  If this gets overloaded with too many messages then will need to throttle this down by preventing calls to 
+#  writething() more frequently than every half second and recording a set() of topics which are to change
+#
+#  We might needs some special full-width lines that show the current status of each ESPURNA so they don't 
+#  take up all the space for the more interesting MQTT signals 
+# 
+#  Hit 'q' to quit and 's' to sort by most recent message.
+
+
+broker_address="mqtt.local"
+reignoredtopics = re.compile("ESPURNA......./(?:factor|energy|voltage|apparent|vcc|freeheap|current|reactive|version|host|loadavg|mac)")
 
 msgdict = { }
 topiclinedict = { }
@@ -32,10 +51,8 @@ def sortwritethings(stdscr):
     stdscr.clear()
     for topic in msgdict:
         writething(topic)
-        #break
     stdscr.refresh()
 
-reignoredtopics = re.compile("ESPURNA......./(?:factor|energy|voltage|apparent|vcc|freeheap|current|reactive|version|host|loadavg|mac)")
 
 def on_message(client, userdata, message):
     if height == 0:
@@ -55,7 +72,6 @@ def on_message(client, userdata, message):
     writething(topic)
     client.stdscr.refresh()
     
-broker_address="mqtt.local"
 print("creating new instance")
 client = mqtt.Client("P1") 
 client.on_message = on_message 
@@ -81,4 +97,5 @@ def main(stdscr):
 client.loop_start() 
 client.subscribe("#")
 curses.wrapper(main)
+curses.endwin()
 client.loop_stop()
