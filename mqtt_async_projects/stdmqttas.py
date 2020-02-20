9f31ba9e31d830278bcbb462973c5be3234319d3
Julian Todd <julian@goatchurch.org.uk>
2020-02-20 12:27:40 +0000

    work on ledpanel and mqttas

--- mqtt_async_projects/stdmqttas.py
+++ mqtt_async_projects/stdmqttas.py
@@ -1,16 +1,25 @@
-import time, network
+import time, network, sys
 from machine import Pin
 from mqtt_as import config
 import uasyncio as asyncio
 from mqtt_as import config
     
 fconfig = dict(x.strip().split(None, 1)  for x in open("config.txt"))
-config['server'] = fconfig["mqttbroker"]
-config['ssid'] = fconfig["wifiname"]
-config['wifi_pw'] = fconfig["wifipassword"]
+cdelimeter = fconfig.get("cdelimeter", ",")
+if "connection0" in fconfig:
+    config['ssid'], config['wifi_pw'], config['server'] = fconfig["connection0"].split(cdelimeter)
+else:
+    config['ssid'] = fconfig["wifiname"]
+    config['wifi_pw'] = fconfig["wifipassword"]
+    config['server'] = fconfig["mqttbroker"]
+
 pinled = Pin(abs(int(fconfig["pinled"])), Pin.OUT)  if "pinled" in fconfig  else None
 shortmac = "{:02X}{:02X}{:02X}".format(*network.WLAN().config('mac')[-3:])
-#network.WLAN().active(0)  # disable the connection at startup
+topicstem = "%s/%s" % (sys.platform, shortmac)
+network.WLAN().active(0)   # disable the connection at startup
+topicstatus = topicstem+"/status"
+config['will'] = (topicstatus, "offline", True)
+config['ping_interval'] = 30
 
 def flashpinled(n=5, sl0=300, sl1=300):
     if pinled:
@@ -27,16 +36,21 @@ async def flashledconnectedtask(client):
         pinled.value(1-pinledOnvalue)
         await asyncio.sleep_ms(5000)
 
-async def mqttconnecttask(client, bflip=False):
-    if bflip and "wifialt" in fconfig:
-        assid, awifi_pw, aserver = fconfig["wifialt"].split(",")
-        if client._ssid == assid:
-            assid, awifi_pw, aserver = fconfig["wifiname"], fconfig["wifipassword"], fconfig["mqttbroker"]
-        client._ssid, client._wifi_pw, client.server = assid, awifi_pw, aserver
+async def mqttconnecttask(client, cnumber=None):
+    if cnumber is not None:
+        if ("connection%d"%cnumber) not in fconfig:
+            cnumber = 0
+        client._ssid, client._wifi_pw, client.server = \
+            fconfig.get("connection%d"%cnumber, \
+            cdelimeter.join(fconfig.get(x, 'x') for x in ['wifiname', 'wifipassword', 'mqttbroker']))\
+            .split(cdelimeter)
+    else:
+        cnumber = 0
     try:
         print("await connecting to:", client._ssid, client.server)
         await client.connect()
         print("*** connected:")
+        await client.publish(topicstatus, "connected", retain=True)
         flashpinled(10, 100, 100)
         return
     except OSError as e:
@@ -44,7 +58,8 @@ async def mqttconnecttask(client, bflip=False):
         
     flashpinled(5, 300, 50)
     aloop = asyncio.get_event_loop()
-    aloop.create_task(mqttconnecttask(client, not bflip))
+    aloop.create_task(mqttconnecttask(client, cnumber+1))
+    
 
 # mosquitto_pub -h mqtt.local -t "esp8266maggas1/cmd" -m "print('hi there')"   
 async def callbackcmdtask(client, topicreply, codemsg):
